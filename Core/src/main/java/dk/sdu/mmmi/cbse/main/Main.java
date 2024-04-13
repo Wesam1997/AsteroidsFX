99a179c01fabe519241fb04f4c939fa7c657649f
bruger <Wesam.ba12@gmail.com>
2024-04-13 14:55:21 +0200

    MoudelFinder moudeler

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -10,6 +10,7 @@ import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 import java.lang.module.ModuleFinder;
 import java.lang.module.ModuleReference;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import static java.util.stream.Collectors.toList;
@@ -22,17 +23,38 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-     public class Main extends Application {
+public class Main extends Application {
 
     private final GameData gameData = new GameData();
     private final World world = new World();
     private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
     private final Pane gameWindow = new Pane();
+    private static final List<IGamePluginService> saveIGamePluginService = new ArrayList<>();
+    private static final List<IPostEntityProcessingService> saveIpostEntity = new ArrayList<>();
+    private static final List<IEntityProcessingService> saveIEntityProcessingService = new ArrayList<>();
 
     public static void main(String[] args) {
+        var layer = createLayer(args[0], "ModuleSplit");
 
+        // hente de forskellige layer fra Interface
+
+        ServiceLoader<IGamePluginService> gamePluginServices = ServiceLoader.load(layer, IGamePluginService.class);
+        ServiceLoader<IPostEntityProcessingService> postEntityProcessingServices = ServiceLoader.load(layer, IPostEntityProcessingService.class);
+        ServiceLoader<IEntityProcessingService>iEntityProcessingServices=ServiceLoader.load(layer,IEntityProcessingService.class);
+
+        //stream henter all leyer fra IGamePluginService og lave en map og set den i en varibel som hedder saveIGamePluginService og add til ServiceLoader
+        gamePluginServices.stream().map(ServiceLoader.Provider::get).forEach(saveIGamePluginService::add);
+        postEntityProcessingServices.stream().map(ServiceLoader.Provider::get).forEach(saveIpostEntity::add);
+        iEntityProcessingServices.stream().map(ServiceLoader.Provider::get).forEach(saveIEntityProcessingService::add);
         launch(Main.class);
     }
+         private static ModuleLayer createLayer(String from, String module) {
+             var finder = ModuleFinder.of(Paths.get(from));
+             var parent = ModuleLayer.boot();
+             var cf = parent.configuration().resolve(finder, ModuleFinder.of(), Set.of(module));
+             return parent.defineModulesWithOneLoader(cf, ClassLoader.getSystemClassLoader());
+
+         }
 
     @Override
     public void start(Stage window) throws Exception {
@@ -75,11 +97,18 @@ import javafx.stage.Stage;
         for (IGamePluginService iGamePlugin : getPluginServices()) {
             iGamePlugin.start(gameData, world);
         }
+        for (IGamePluginService iGamePluin : saveIGamePluginService){
+            iGamePluin.start(gameData,world);
+        }
+
+
         for (Entity entity : world.getEntities()) {
             Polygon polygon = new Polygon(entity.getPolygonCoordinates());
             polygons.put(entity, polygon);
             gameWindow.getChildren().add(polygon);
+
         }
+
         render();
         window.setScene(scene);
         window.setTitle("ASTEROIDS");
@@ -102,9 +131,15 @@ import javafx.stage.Stage;
         for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
             entityProcessorService.process(gameData, world);
         }
+        for (IEntityProcessingService iEntityProcessingService : saveIEntityProcessingService){
+            iEntityProcessingService.process(gameData,world);
+        }
         for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
             postEntityProcessorService.process(gameData, world);
-        }       
+        }
+        for (IPostEntityProcessingService postEntityProcessingService : saveIpostEntity){
+            postEntityProcessingService.process(gameData,world);
+        }
     }
 
     private void draw() {        
@@ -127,7 +162,6 @@ import javafx.stage.Stage;
             polygon.setTranslateY(entity.getY());
             polygon.setRotate(entity.getRotation());
         }
-
     }
 
     private Collection<? extends IGamePluginService> getPluginServices() {
