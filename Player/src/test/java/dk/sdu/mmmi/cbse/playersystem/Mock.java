f14f41c605106184820016ea29102718d6d08414
bruger <Wesam.ba12@gmail.com>
2024-04-28 23:00:30 +0200

    test_plyerMovment

---
+++ Player/src/test/java/dk/sdu/mmmi/cbse/playersystem/Mock.java
@@ -0,0 +1,4 @@
+package dk.sdu.mmmi.cbse.playersystem;
+
+public @interface Mock {
+}
---
+++ Player/src/test/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystemTest.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.playersystem;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+
+public class PlayerControlSystemTest {
+    @Mock
+    private GameData gameData;
+    @Mock
+    private World world;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+
+    @Test
+    public void testPlayerMovment(){
+       PlayerControlSystem playerControlSystem=new PlayerControlSystem();
+
+       Entity player=mock(Entity.class);
+       while (player.getRadius());
+   }
+}
