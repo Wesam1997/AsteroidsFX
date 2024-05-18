a24aa173c360c64cf253e6497fa2694f186b0644
bruger <Wesam.ba12@gmail.com>
2024-05-19 00:33:48 +0200

    MicroTest

--- ScoringSystem/src/main/java/ScoringSystem.java
+++ ScoringSystem/src/main/java/ScoringSystem.java
@@ -4,18 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
 @RestController
 
 public class ScoringSystem {
-
     private Long totalScore = 0L;
-
     public static void main(String[] args) {
         SpringApplication.run(ScoringSystem.class, args);
     }
-
     @GetMapping("/score")
     public Long calculateHealt(@RequestParam(value = "point") Long point) {
         totalScore += point;
--- ScoringSystem/src/test/java/ScoringSystemTest.java
+++ ScoringSystem/src/test/java/ScoringSystemTest.java
@@ -6,6 +6,7 @@ class ScoringSystemTest {
 
     @Test
     void contextLoads() {
+
     }
 
 }
